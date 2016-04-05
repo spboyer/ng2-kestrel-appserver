17241552da6cc57d6e50caf69f28e980862af2b4
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2016-04-05 11:21:48 -0400

    add IHostingEnvironment back

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,15 @@ namespace ng2_kestrel_appserver
     {
         public IConfigurationRoot Configuration { get; set; }
 
+public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
@@ -19,10 +29,14 @@ namespace ng2_kestrel_appserver
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(LogLevel.Debug);
 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             /// http://docs.asp.net/en/latest/fundamentals/static-files.html
             // need to write a middleware piece potentially and read up more here
             DefaultFilesOptions options = new DefaultFilesOptions();
