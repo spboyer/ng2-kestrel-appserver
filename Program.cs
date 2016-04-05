6f09e2b3e6e667639ef12ce1985dc9c005232d0f
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2016-04-05 10:57:04 -0400

    static server conversion to dotnet-cli

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace ng2_kestrel_appserver
+{
+
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseDefaultHostingConfiguration(args)
+                        //.UseIIS()
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -11,37 +10,20 @@ namespace ng2_kestrel_appserver
     {
         public IConfigurationRoot Configuration { get; set; }
 
-        public Startup(IHostingEnvironment env)
-        {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
-                //.SetBasePath(appEnv.ApplicationBasePath)
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddConsole(LogLevel.Debug);
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("error.html");
-            }
+
 
             /// http://docs.asp.net/en/latest/fundamentals/static-files.html
             // need to write a middleware piece potentially and read up more here
@@ -51,7 +33,7 @@ namespace ng2_kestrel_appserver
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
 
-            app.UseIISPlatformHandler();
+            //app.UseIISPlatformHandler();
 
             /*            app.UseMvc(routes =>
                          {
@@ -63,6 +45,6 @@ namespace ng2_kestrel_appserver
         }
 
         // Entry point for the application.
-        public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
+        //public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
