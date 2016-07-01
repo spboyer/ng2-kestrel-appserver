54938418704597b060ae62697e595c24a9669b1f
Shayne Boyer <shayne.p.boyer@disney.com>
2016-06-30 21:25:30 -0400

    update for dotnet core rtm and ng2-rc4

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 
--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -1,10 +1,11 @@
-import {Component} from 'angular2/core';
+import { Component } from '@angular/core';
 
 @Component({
-	selector: 'my-app',
-	template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
+
 
 /*
 Copyright 2016 Google Inc. All Rights Reserved.
